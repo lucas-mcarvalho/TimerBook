32362cd8e7b8e01a3ae72339e36698d98f664bfe
Bruno-uft <bruno.sobral@mail.uft.edu.br>
2025-09-27 15:32:00 -0300

    Modificações na tela Home

--- App/views/html/home.php
+++ App/views/html/home.php
@@ -10,12 +10,15 @@
 
     <header class="main-header">
         <div class="header-logo">
-            <img src="uploads/main_logo.png" alt="Logo Timerbook" class="logo-img">
-            <h1>TimerBook</h1>
+            <a href="index.php?action=home" class="logo-link">
+                <img src="uploads/main_logo.png" alt="Logo Timerbook" class="logo-img">
+                <h1>TimerBook</h1>
+            </a>
         </div>
         <div class="header-profile">
             <img src=" " alt="Foto de Perfil" class="profile-pic">
-            <button class="profile-button">Meu Perfil</button>
+            <a href="index.php?action=perfil" class="profile-button">Meu Perfil</a>
+            <a href="index.php?action=sair" class="logout-button">Encerrar Sessão</a>
         </div>
     </header>
 
@@ -32,17 +35,17 @@
                         <img src="uploads/boasvindas_carrossel.png" alt="Imagem 1">
                     </div>
                     <div class="slide">
-                        <a href="index.php?action=cadastrolivro">
+                        <a href="index.php?action=Adicionar_Livro">
                             <img src="uploads/aindanaocadastrou.png" alt="Imagem 2">
                         </a>
                     </div>
                     <div class="slide">
-                        <a href="index.php?action=perfil">
+                        <a href="index.php?action=adm_editar">
                             <img src="uploads/acessoperfil.png" alt="Imagem 3">
                         </a>
                     </div>
                     <div class="slide">
-                        <a href="index.php?action=reset_password">
+                        <a href="index.php?action=forgot_password">
                             <img src="uploads/redefinasuasenha.png" alt="Imagem 4">
                         </a>
                     </div>
@@ -68,9 +71,9 @@
     </main>
 
     <footer class="main-footer">
-        <button class="add-book-button">
-            <span class="plus-icon">+</span> Cadastrar Novo Livro
-        </button>
+        <a href="index.php?action=Adicionar_Livro" class="add-book-button">
+            <span class="plus-icon">+</span> Cadastrar Livro
+        </a>
     </footer>
 
     <script>
